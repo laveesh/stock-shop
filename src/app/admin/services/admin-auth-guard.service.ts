4351b595e558b6e2eaae0feab5b7beb78838a40e
Laveesh Gupta <loveeshgoyal@gmail.com>
2020-05-23 03:47:57 +0530

    Add: service for auth guard admin route

---
+++ src/app/admin/services/admin-auth-guard.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { UserService } from 'src/app/shared/services/user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminAuthGuard implements CanActivate {
+  constructor(private auth: AuthService, private userService: UserService) {}
+
+  canActivate(): Observable<boolean> {
+    return this.auth.appUser$.pipe(map(appUser => appUser.isAdmin));
+  }
+}
